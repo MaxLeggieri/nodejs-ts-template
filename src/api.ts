84b3be9d4a2987e0bf4a850fee02141e32a983a9
Max Leggieri <maxleggieri@gmail.com>
2023-10-29 01:47:00 +0200

    Added cors, json and compression

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,13 @@
-import express from 'express'
+import express from "express"
+import compression from "compression"
+import cors from "cors"
+
 const app = express()
 const port = 3000
+
+app.use(cors())
+app.use(express.json())
+app.use(compression())
 
 app.get('/', (req, res) => {
   res.json({
@@ -12,3 +19,5 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`)
 })
+
+
